138ce897d0384b4b3fd4f0cffc2257be627763a2
MarlyssaB <marlyssa.boucher@gmail.com>
2019-06-04 20:53:19 -0500

    First stages of tictactoe

---
+++ MyPortfolio/scripts/ticTacToe.js
@@ -0,0 +1,8 @@
+var gameMarker = "x"
+
+function changeMarkerToX(){
+    console.log("The x button was clicked!");
+ }
+function changeMarkerToO(){
+    console.log("The x button was clicked!");
+ }
