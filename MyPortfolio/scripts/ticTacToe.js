dca12ca466949bc2fc6ce42b550344b753cf5532
MarlyssaB <marlyssa.boucher@gmail.com>
2019-06-06 19:39:14 -0500

    Tic Tac Toe files- complete

--- MyPortfolio/scripts/ticTacToe.js
+++ MyPortfolio/scripts/ticTacToe.js
@@ -6,3 +6,14 @@ function changeMarkerToX(){
 function changeMarkerToO(){
     console.log("The x button was clicked!");
  }
+ var xturn=true;
+ function placemarker(id){
+    var thisSquare = document.getElementById(id); //thisSquare is the clicked/selected square
+    if (xturn){
+        thisSquare.innerHTML = "X";
+    }
+    else{
+        thisSquare.innerHTML = "O";
+    }
+    xturn = !xturn;
+ }
